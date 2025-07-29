24b3452ad31cdea807210aa8e8ae7ad1d7373fbc
SK-Digital <121402190+SK-Digital@users.noreply.github.com>
2025-07-29 11:42:43 +0100

    Add Debian kiosk installer script

---
+++ installer.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# be new
+apt-get update
+
+# get software
+apt-get install \
+    unclutter \
+    xorg \
+    chromium \
+    openbox \
+    lightdm \
+    locales \
+    -y
+
+# dir
+mkdir -p /home/kiosk/.config/openbox
+
+# create group
+groupadd -f kiosk
+
+# create user if not exists
+id -u kiosk &>/dev/null || useradd -m kiosk -g kiosk -s /bin/bash 
+
+# rights
+chown -R kiosk:kiosk /home/kiosk
+
+# remove virtual consoles
+if [ -e "/etc/X11/xorg.conf" ]; then
+    mv /etc/X11/xorg.conf /etc/X11/xorg.conf.backup
+fi
+cat > /etc/X11/xorg.conf << EOF
+Section "ServerFlags"
+    Option "DontVTSwitch" "true"
+EndSection
+EOF
+
+# create config
+if [ -e "/etc/lightdm/lightdm.conf" ]; then
+    mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.backup
+fi
+cat > /etc/lightdm/lightdm.conf << EOF
+[Seat:*]
+xserver-command=X -nolisten tcp
+autologin-user=kiosk
+autologin-session=openbox
+EOF
+
+# create autostart
+if [ -e "/home/kiosk/.config/openbox/autostart" ]; then
+    mv /home/kiosk/.config/openbox/autostart /home/kiosk/.config/openbox/autostart.backup
+fi
+cat > /home/kiosk/.config/openbox/autostart << EOF
+#!/bin/bash
+
+KIOSK_URL="https://wallboard.x-onweb.com"
+
+# The original script included 'unclutter -idle 0.1 -grab -root &'.
+# We are removing it to keep the mouse visible.
+
+while :
+do
+    xrandr --auto
+    chromium \
+        --noerrdialogs \
+        --no-memcheck \
+        --no-first-run \
+        --start-maximized \
+        --disable \
+        --disable-translate \
+        --disable-infobars \
+        --disable-suggestions-service \
+        --disable-save-password-bubble \
+        --disable-session-crashed-bubble \
+        --incognito \
+        --kiosk $KIOSK_URL
+    sleep 5
+done &
+EOF
+
+echo "Done!"
